95f27bba85a71c09022cc569130358e60e0fe465
Monse0051 <rouge0051@gmail.com>
2019-03-21 21:27:39 -0600

    * Agregamos archivo .gitignore para evitar subir archivos a git (config.js) * Movimos variable config a src/config.js (archivo no versionado) * Agregamos carpeta assets para logos * Creamos estilos de CSS * Integramos al codigo HTML pagina de logueo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
-function initApp() {
+/**
+ * Initializes Social Network application
+ * @param {Object} config configuration data from firebase
+ */
+function initApp(config) {
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAG-k8sfR7HI-GHNFHv5mQlfb4Bu8zvJIY",
-    authDomain: "social-network-a702c.firebaseapp.com",
-    databaseURL: "https://social-network-a702c.firebaseio.com",
-    projectId: "social-network-a702c",
-    storageBucket: "social-network-a702c.appspot.com",
-    messagingSenderId: "99341463255"
-  };
+  
   firebase.initializeApp(config);
 
   // Get elements
@@ -18,7 +15,7 @@ function initApp() {
   const btnLogOut = document.getElementById("btnLogOut");
 
   // add login Event
-  btnLogin.addEventListener("click", function(event) {
+  btnLogin.addEventListener("click", function (event) {
     // Get email and password
     console.log("DEBUG_MSG: login event");
     const email = txtEmail.value;
@@ -26,41 +23,50 @@ function initApp() {
     const auth = firebase.auth();
     // sign in
     const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch( event => console.log(event.message));
+    promise.catch(event => console.log(event.message));
   });
 
   // add sign up event
   btnSignUp.addEventListener("click", event => {
     // Get email and pass
     //TODO: check for real email
+    console.log("DEBUG_MSG: sign up event");
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
     // sign in
     const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(e =>console.log(e.message));
-});
+    promise.catch(e => console.log(e.message));
+  });
 
-// add log out event listener
-btnLogOut.addEventListener("click", event => {
+  // add log out event listener
+  btnLogOut.addEventListener("click", event => {
     firebase.auth().signOut();
-});
+  });
 
-// add realtime listener
-firebase.auth().onAuthStateChanged(firebaseUser => {
+  // add realtime listener
+  firebase.auth().onAuthStateChanged(firebaseUser => {
     // TODO create functions handleSignedInUser and handleSignedOutUser
+    console.log("DEBUG_MSG: auth state change event");
     if (firebaseUser) {
-        console.log(firebaseUser);
-        btnLogOut.style.visibility = "visible";
-        //handleSignedInUser(firebaseUser);
+      console.log(firebaseUser);
+      btnLogOut.style.visibility = "visible";
+      //handleSignedInUser(firebaseUser);
     } else {
-        console.log("not logged in");
-        btnLogOut.style.visibility = "hidden";
-        //handleSignedOutUser(firebaseUser)
+      console.log("not logged in");
+      btnLogOut.style.visibility = "hidden";
+      //handleSignedOutUser(firebaseUser)
     }
-});
+  });
 
 };
 
-initApp();
+initApp(config);
 
+// window.addEventListener("click", function (event) {
+//   let modal = document.getElementById('id01');
+//   if (event.target === modal ) {
+//     modal.style.display = "none";
+//   }
+  
+// });
