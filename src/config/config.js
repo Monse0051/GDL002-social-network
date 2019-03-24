231c4e10ccf46db858051cfa12f204d2fb3b51ee
Monse0051 <rouge0051@gmail.com>
2019-03-23 21:20:49 -0600

    Simplificamos html para empezar en limpio. * Limpiamos CSS * Agregamos mensajes descriptivos de error en app.js * Agregamos archivo de configuracion de firebase

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+ // Initialize Firebase
+ let config = {
+    apiKey: "AIzaSyAG-k8sfR7HI-GHNFHv5mQlfb4Bu8zvJIY",
+    authDomain: "social-network-a702c.firebaseapp.com",
+    databaseURL: "https://social-network-a702c.firebaseio.com",
+    projectId: "social-network-a702c",
+    storageBucket: "social-network-a702c.appspot.com",
+    messagingSenderId: "99341463255"
+  };
