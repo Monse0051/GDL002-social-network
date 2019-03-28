c1b7aa1988e8a9aee557390e1a1447e34103a6d4
ClaudiaGarfias <me@claudiagarfias.works>
2019-03-28 10:25:19 -0600

    Inyectamos contenido de la timeline screen al DOM desde controller.js , está comentado

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,21 @@
 function handleSignedInUser(firebaseUser) {
     location.hash = "#timeline";
+    // contentDiv.innerHTML = 
+    // `<nav id = "timeline-nav" class= "timeline-nav flex-row">
+    //     <h1>madame{code}</h1>
+    //     <button id = "btnLogOut">Salir</button>
+    // </nav>
+        
+    // <article id="create-post" class="create-post">
+    //     <h2>Crear Publicación:</h2>
+    //     <input id="input-post" type="text" placeholder="¿Qué quieres compartir con la comunidad?">
+    //     <p>Visible para:</p>
+    //     <input type="radio" name="public" value="public"> Público
+    //     <input type="radio" name="friends" value="friends"> Sólo Amigos
+    //     <br>
+    //     <button>Publicar</button>
+    // </article>
+    // `;
 }
 
 function handleSignedOutUser() {
