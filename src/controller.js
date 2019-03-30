1b4bf65a57c1ab712144c10eed2506c48f4b84da
ClaudiaGarfias <me@claudiagarfias.works>
2019-03-30 07:11:22 -0600

    Se regresó login screen al div content y ese contenido se reemplaza con timeline screen cada vez que el usuario inicia sesión. No se ha probado si el contenido inyectado de esta forma tiene funcionalidad ocn firebase

--- src/controller.js
+++ src/controller.js
@@ -1,15 +1,8 @@
 function handleSignedOutUser() {
     location.hash = "#login";
-    contentDiv.innerHTML =  
-    `<section class = "login-screen">
-        <article id = "error"></article>
-        <input id = "txtEmail" type="email" placeholder="Email"> 
-        <input id = "txtPassword" type="password" placeholder= "Contraseña">
-        <button id = btnLogin class="btn-btnb-action">Iniciar sesión</button>
-        <button id = btnSignUp class="btn-btn-secondary">Crear Cuenta</button>
-        <button id = btnLogOut class="btn-btn-action-hide">Cerrar Sesión</button> 
-    </section>
-    `;    
+    // contentDiv.innerHTML =  
+    // `
+    // `;    
 }
 
 //pienso que se puede crear una función pura que maneje estas dos
@@ -18,22 +11,42 @@ function handleSignedOutUser() {
 function handleSignedInUser(firebaseUser) {
     location.hash = "#timeline";
     contentDiv.innerHTML = 
-    `<nav id = "timeline-nav" class= "timeline-nav flex-row">
-        <h1>madame{code}</h1>
-        <button id = "btnLogOut">Salir</button>
-    </nav>
-        
-    <article id="create-post" class="create-post">
-        <h2>Crear Publicación:</h2>
-        <input id="input-post" type="text" placeholder="¿Qué quieres compartir con la comunidad?">
-        <p>Visible para:</p>
-        <input type="radio" name="public" value="public"> Público
-        <input type="radio" name="friends" value="friends"> Sólo Amigos
-        <br>
-        <button>Publicar</button>
+    `<section class = "timeline-screen">
+        <nav id = "timeline-nav" class= "timeline-nav flex-row">
+            <h1>madame{code}</h1>
+            <a href="">Salir<img src=""></a>
+        </nav>
 
-        <button id = btnLogOut class="btn-btn-action-hide">Cerrar Sesión</button> 
-    </article>
+        <article id="create-post" class="create-post">
+            <h2>Crear Publicación:</h2>
+            <input id="input-post" type="text" placeholder="¿Qué quieres compartir con la comunidad?">
+            <p>Visible para:</p>
+            <input type="radio" name="public" value="public"> Público
+            <input type="radio" name="friends" value="friends"> Sólo Amigos
+            <br>
+            <button>Publicar</button>
+        </article>
+    </section>
+
+    <section id="all-posts">
+        <h2>Publicaciones recientes:</h2>
+        <article id="display-posts">
+            <div id ="own-post">
+                <p id="user-email"><span class="strong">micorreo@es.com</span> compartió:</p><button id="edit-post">Edit</button>
+                <br>
+                <p id="publication" contenteditable="false">Texto EDITABLE de la publicación:</p>
+                <br>
+                <button id="like-post">like</button>
+            </div>
+            <div id ="other-post">
+                    <p id="user-email"><span class="strong">correodeotros@es.com</span> compartió:</p>
+                    <br>
+                    <p id="publication">Texto de la publicación:</p>
+                    <br>
+                    <button id="like-post">like</button>
+            </div>
+        </article>
+    </section>
     `;
 }
 
