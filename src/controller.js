214d4d0d973f6034167373475ac6fe5fbacdcf04
ClaudiaGarfias <me@claudiagarfias.works>
2019-04-01 20:22:57 -0600

    Ya se puede postear y guardar en db. Se agregaron métodos add y then a la función de Monse para checar errores y traer id de post. Creo que con esto también se puede pintar el post recién realizado en el DOM

--- src/controller.js
+++ src/controller.js
@@ -11,9 +11,14 @@ const handleSignedOutUser = () => {
 
 //pienso que se puede crear una función pura que maneje estas dos
 // también sugiero cambairles el nombre
+const USERS_COLLECTION = "users_tests_monse";
 
 const handleSignedInUser = (firebaseUser) => {
     //location.hash = "#timeline";
+    let userEmail = firebaseUser.email;
+    let userName = firebaseUser.displayName;
+    let userId = firebaseUser.uid;
+
     contentDiv.innerHTML = 
     `<section class = "timeline-screen">
         <nav id = "timeline-nav" class= "timeline-nav flex-row">
@@ -30,7 +35,7 @@ const handleSignedInUser = (firebaseUser) => {
             <input type="radio" name="public" value="public"> Público
             <input type="radio" name="friends" value="friends"> Sólo Amigos
             <br>
-            <button>Publicar</button>
+            <button id ="button-post">Publicar</button>
         </article>
     </section>
 
@@ -38,7 +43,7 @@ const handleSignedInUser = (firebaseUser) => {
         <h2>Publicaciones recientes:</h2>
         <article id="display-posts">
             <div id ="own-post">
-                <p id="user-email"><span class="strong">${firebaseUser.email}</span> compartió:</p><button id="edit-post">Edit</button>
+                <p id="user-email"><span class="strong">${userEmail}</span> compartió:</p><button id="edit-post">Edit</button>
                 <br>
                 <p id="publication" contenteditable="false">Texto EDITABLE de la publicación:</p>
                 <br>
@@ -54,9 +59,45 @@ const handleSignedInUser = (firebaseUser) => {
         </article>
     </section>
     `;
+
+    console.log(userEmail);
+    document.getElementById("button-post").addEventListener("click", function () {
+        let post = document.getElementById("input-post").value;
+        if (post.length >0) {
+            //TODO: extract if post is public or not from radio box
+            
+            db.collection(`${USERS_COLLECTION}/user_${userEmail}/Posts FROM:${userEmail}/`)
+            .add(CreatePost(userEmail, post, true))
+            .then(function(docRef) {
+                console.log("Document written with ID: ", docRef.id); //DOCUMENT ID
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error.code);//Catches ERROR
+            });
+        }
+
+        else{
+            // TODO show message that post is empty
+        }
+
+    });
+
+
     const userInfo = document.getElementById('user-info');
-    userInfo.innerHTML =    `ID de usuario: ${firebaseUser.uid}<br>
-                            Email: ${firebaseUser.email}<br>
-                            Nombre: ${firebaseUser.displayName}`;
+    userInfo.innerHTML =    `ID de usuario: ${userId}<br>
+                            Email: ${userEmail}<br>
+                            Nombre: ${userName}`;
+
+
+
 }
 
+const CreatePost = (mail, textval, isPublic) => {
+    //TODO make a function to return current date
+    return {
+        email:mail,
+        text: textval,
+        is_public: isPublic,
+        date : "01/04/2019"
+    };
+}
