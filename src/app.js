0bf17ea2a3779ca4276777ddb0f248169bd963c6
Monse0051 <rouge0051@gmail.com>
2019-04-03 10:11:36 -0600

    PEER PROGRAMING Creamos autenticación con google.

--- src/app.js
+++ src/app.js
@@ -110,10 +110,34 @@ firebase.auth().onAuthStateChanged( function(firebaseUser) {
     handleSignedOutUser();
   }
 });
-  
 
+const signInGoogleBtn = document.getElementById('sign-in-google');
+// Set GOOGLE as provider
+let provider = new firebase.auth.GoogleAuthProvider();
 
-// Navigate whenever the fragment identifier value changes.
-// TODO
-window.addEventListener("hashchange", router);
-window.addEventListener("load", router);
+//Get GOOGLE ACCOUNT Authentication FIREBASE DOCUMENTATION
+const googleSignin = () => {
+ firebase.auth().signInWithPopup(provider).then(function(result) {
+ // This gives you a Google Access Token. You can use it to access the Google API.
+ var token = result.credential.accessToken;
+ // The signed-in user info.
+ var user = result.user;
+ // ...
+}).catch(function(error) {
+ // Handle Errors here.
+ var errorCode = error.code;
+ var errorMessage = error.message;
+ // The email of the user's account used.
+ var email = error.email;
+ // The firebase.auth.AuthCredential type that was used.
+ var credential = error.credential;
+ // ...
+});
+}
+
+signInGoogleBtn.addEventListener("click", googleSignin );
+
+// // Navigate whenever the fragment identifier value changes.
+// // TODO
+// window.addEventListener("hashchange", router);
+// window.addEventListener("load", router);
